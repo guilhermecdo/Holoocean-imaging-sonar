deb887a12bbe8fc6cfc319c14ff9026681158094
guilhermecdo <guilherme@furg.br>
2024-03-12 17:12:32 -0300

    delete old scripts

--- install.py
+++
@@ -1,6 +0,0 @@
-import  holoocean
-#oloocean.install("Ocean", branch="develop")
-
-holoocean.packagemanager.prune()
-print(holoocean.packagemanager.available_packages())
-print(holoocean.packagemanager.installed_packages())
