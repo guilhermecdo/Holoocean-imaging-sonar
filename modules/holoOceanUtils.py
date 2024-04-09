b540f17566be896fa17ace0e5acb43fd933286d9
guilhermecdo <guilherme@furg.br>
2024-04-09 14:55:05 -0300

    PID control

--- modules/holoOceanUtils.py
+++ modules/holoOceanUtils.py
@@ -19,11 +19,28 @@ class scenario:
         self.cfg["agents"].append(agent) 
         pass
 
-class ROV:
+class PIDController:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.prev_error = 0
+        self.integral = 0
+
+    def update(self, error, dt):
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt
+        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        self.prev_error = error
+        return output
+
+class AUV:
     def __init__(self, id:str,control_scheme:int=1,location=[int,int,int],rotation=[int,int,int])->None:
         self.id=id
         self.name:str="auv"+str(id)
         self.control_scheme=control_scheme
+        self.start_location=location
+        self.start_rotation=rotation
 
         self.number_of_sensors:int=0
         self.sonar_ID:int
@@ -33,10 +50,28 @@ class ROV:
             "sensors":[
             ],
             "control_scheme":self.control_scheme,
-            "location": location,
-            "rotation": rotation
+            "location": self.start_location,
+            "rotation": self.start_rotation
         }
 
+        self.waypoints=[]
+        self.number_of_waypoints:int=0
+        self.reached_waypoints:int=0
+        self.actual_waypoint=[]
+
+        self.distance_tresh_hold=0.1
+        self.angle_tresh_hold=2.0
+        
+        self.sonar_image=None
+        self.actual_location=location
+        self.actual_rotation=rotation
+
+        self.pid_controller_linear = PIDController(2.5,0,0.01)
+        self.pid_controller_angular = PIDController(0.01,0,0.001)
+        self.dt=1/200
+
+        self.command=None
+        
     def addSensor(self,sensor:str,socket:str)->None:
         self.agent["sensors"].append({"sensor_type":sensor,
                                     "socket": socket})
@@ -100,19 +135,45 @@ class ROV:
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
     
-    def updateImnage(self,sonarImage)->None:
-        s = sonarImage
+    def updateSonarImage(self)->None:
+        s = self.sonar_image
         self.plot.set_array(s.ravel())
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
+    
+    def updateState(self,state)->None:
+        if 'ImagingSonar' in state[self.name]:
+            self.sonar_image=(state[self.name]['ImagingSonar'])
+            self.updateSonarImage()
+        if 'LocationSensor' in state[self.name]:
+            self.actual_location=(state[self.name]['LocationSensor'])
+        if 'RotationSensor' in state[self.name]:
+            self.actual_rotation=(state[self.name]['RotationSensor'])
+        
+        self.reachedWaypoint()
+        self.calculateVelocities()
 
-    def reachedWaypoint(self,waypoint:list,rov_location:list,rov_rotation:list,tresh_hold_distance:float,tresh_hold_angle:float)->bool:
-        x_rov=rov_location[0]
-        y_rov=rov_location[1]
-        z_rov=rov_location[2]
-        roll_rov=rov_rotation[0]
-        pitch_rov=rov_rotation[1]
-        yaw_rov=rov_rotation[2]
+    def createWaypoints(self,end_z)->None:
+        angles=np.linspace(0,350,36)
+        headings=np.concatenate((np.linspace(180,350,18),np.linspace(0,170,18)), axis=None)
+        elevation=np.arange(-2.5,end_z,0.3 )
+        for z in elevation:
+            for angle, heading in zip(angles,headings):
+                x=2*np.cos(np.deg2rad(angle))+self.start_location[0]-2
+                y=2*np.sin(np.deg2rad(angle))
+                self.waypoints.append([x,y,z,0,0,heading])
+        self.number_of_waypoints=len(self.waypoints)
+        self.actual_waypoint=self.waypoints[0]
+
+    def reachedWaypoint(self)->bool:
+        tresh_hold_distance=self.distance_tresh_hold
+        tresh_hold_angle=self.angle_tresh_hold
+        x_rov=self.actual_location[0]
+        y_rov=self.actual_location[1]
+        z_rov=self.actual_location[2]
+        roll_rov=self.actual_rotation[0]
+        pitch_rov=self.actual_rotation[1]
+        yaw_rov=self.actual_rotation[2]
         
         if roll_rov>180:
             roll_rov-=360
@@ -121,12 +182,12 @@ class ROV:
         if yaw_rov>180:
             yaw_rov-=360
 
-        x=waypoint[0]
-        y=waypoint[1]
-        z=waypoint[2]
-        roll=waypoint[3]
-        pitch=waypoint[4]
-        yaw=waypoint[5]
+        x=self.actual_waypoint[0]
+        y=self.actual_waypoint[1]
+        z=self.actual_waypoint[2]
+        roll=self.actual_waypoint[3]
+        pitch=self.actual_waypoint[4]
+        yaw=self.actual_waypoint[5]
 
         if roll>180:
             roll-=360
@@ -136,8 +197,31 @@ class ROV:
             yaw-=360
 
         if np.linalg.norm(np.array([x,y,z])-np.array([x_rov,y_rov,z_rov]))<=tresh_hold_distance and np.linalg.norm(roll-roll_rov)<=tresh_hold_angle and np.linalg.norm(pitch-pitch_rov)<=tresh_hold_angle and np.linalg.norm(yaw-yaw_rov)<=tresh_hold_angle:
-            return True
+           self.reached_waypoints+=1
+           self.actual_waypoint=self.waypoints[self.reached_waypoints]
+           return True
         else:
             return False
         
+    def calculateVelocities(self)->None:
+        position_error = self.actual_waypoint[:3] - self.actual_location
         
+        desired_linear_velocity = self.pid_controller_linear.update(np.linalg.norm(position_error), self.dt)
+        linear_velocity = position_error / np.linalg.norm(position_error) * desired_linear_velocity
+
+        erro_orientacao = self.actual_waypoint[3:] - self.actual_rotation
+
+        erro_orientacao[erro_orientacao > 180] -= 360
+        erro_orientacao[erro_orientacao < -180] += 360
+
+        desired_angular_velocity = self.pid_controller_angular.update(np.linalg.norm(erro_orientacao), self.dt)
+        angular_velocity = erro_orientacao / np.linalg.norm(erro_orientacao) * desired_angular_velocity
+
+        self.command = np.concatenate((linear_velocity, angular_velocity), axis=None)
+
+    def fineshedMission(self)->None:
+        if self.reached_waypoints==self.number_of_waypoints:
+            self.command=[0,0,0,0,0,0]
+            return True
+        else:
+            return False
--- multi-AUV-sonar.py
+++ multi-AUV-sonar.py
@@ -1,58 +1,58 @@
 import holoocean
 import numpy as np
-import matplotlib.pyplot as plt
 import modules.holoOceanUtils
-import os
 
-numberOfROVS=1
+numberOfROVS=2
+auv_array=[]
 
 auv_matrix = [[column for column in range(numberOfROVS)] for row in range(numberOfROVS)]
 
-waypoints=[]
-angles=np.linspace(2*np.pi,0,5)
-for angle in angles:
-    x=2*np.cos(angle)
-    y=2*np.sin(angle)
-    waypoints.append([x,y,-2,0,0,np.rad2deg(angle+(np.pi))])
-
-command=[]
 scenario=modules.holoOceanUtils.scenario("ImagingSonarTank2","64tankMap1","simulationWorld2",200)
 
+for i in range(numberOfROVS):
+    auv_array.append(modules.holoOceanUtils.AUV(str(i),2,[2+(i*7.5),0,-2.5],[0,0,180]))
+    auv_array[i].addSonarImaging()
+    auv_array[i].addSensor("LocationSensor","Origin")
+    auv_array[i].addSensor("RotationSensor","Origin")
+    auv_array[i].imageViwer()
+    auv_array[i].createWaypoints(-1.7)
+    scenario.addAgent(auv_array[i].agent)
+
+"""
 for j in range(numberOfROVS):
     for i in range(numberOfROVS):
-        auv_matrix[j][i]=(modules.holoOceanUtils.ROV(str(j)+str(i),1,[2+(i*7.5),0+(j*-7.5),-2],[0,0,-180]))
-        #auv_matrix[j][i].addSonarImaging()
+        auv_matrix[j][i]=(modules.holoOceanUtils.ROV(str(j)+str(i),2,[2+(i*7.5),(j*-7.5),-2.5],[0,0,180]))
+        auv_matrix[j][i].addSonarImaging()
         auv_matrix[j][i].addSensor("LocationSensor","Origin")
         auv_matrix[j][i].addSensor("RotationSensor","Origin")
-        #auv_matrix[j][i].imageViwer()
-        #command.append([-2.5,-2.5,-2.5,-2.5,0,0,0,0])
+        auv_matrix[j][i].imageViwer()
         scenario.addAgent(auv_matrix[j][i].agent)
-
-fineshed=0
+"""
 
 #start Simulation
 env=holoocean.make(scenario_cfg=scenario.cfg)
 env.reset
 env.set_render_quality(3)
-env.move_viewport([26.25,-26.25,50],[0,0,180])
+#env.move_viewport([26.25,-26.25,50],[0,0,180])
+env.move_viewport([0,0,4],[0,0,180])
+
+
 state = env.tick()
-
-for l in waypoints:
+interactions=0
+for auv in auv_array:
+    for l in auv.waypoints:
+        interactions+=1
         env.draw_point([l[0], l[1], l[2]],[0,255,0], lifetime=0)
-    
-while fineshed<numberOfROVS:
-    for points in waypoints[1:]:       
-        state = env.tick()
-        env._command_center.handle_buffer()
-        while not auv_matrix[j][i].reachedWaypoint(points,state[auv_matrix[j][i].name]["LocationSensor"],state[auv_matrix[j][i].name]["RotationSensor"],0.1,1):
-            os.system("clear")
-            print("ROV Location:",state[auv_matrix[j][i].name]["LocationSensor"],"ROV Waipont:",points[0:3],"ROV Rotation Sensor:",state[auv_matrix[j][i].name]["RotationSensor"],"Angle:",points[3:],end="\r")
-            for j in range(numberOfROVS):
-                for i in range(numberOfROVS):
-                    if 'ImagingSonar' in state[auv_matrix[j][i].name]:
-                        auv_matrix[j][i].updateImnage(state[auv_matrix[j][i].name]['ImagingSonar'])
-                    env.act(auv_matrix[j][i].name, points)
-            state = env.tick()
-    fineshed+=1
 
+
+print(interactions)
+while True:
+    #state = env.tick()
+    for auv in auv_array:
+        auv.updateState(state)
+        if auv.fineshedMission():
+            holoocean.command.RemoveSensorCommand(auv.name,'ImagingSonar')
+        env.act(auv.name,auv.command)
+    state = env.tick()
+        
 print("Finished Simulation!")
